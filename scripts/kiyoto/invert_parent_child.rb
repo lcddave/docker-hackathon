48b7c16da6acc20cdb64e77bad7ca51583033246
vagrant <me@ktamura.com>
2014-06-08 20:21:06 +0000

    update to a denser tree

--- scripts/kiyoto/invert_parent_child.rb
+++ scripts/kiyoto/invert_parent_child.rb
@@ -8,10 +8,13 @@ image_id_to_children = {}
 
 STDIN.each_line { |line|
  data = JSON.parse(line)
- child_id = data["Id"]
- parent_id = data["Parent"]
- image_id_to_children[parent_id] ||= Set.new
- image_id_to_children[parent_id].add(child_id) # upsert
+ ancestry = data["Ancestry"]
+ for i in 0..(ancestry.length-2)
+   child_id = ancestry[i]
+   parent_id = ancestry[i+1]
+   image_id_to_children[parent_id] ||= Set.new
+   image_id_to_children[parent_id].add child_id
+ end
 }
 
 image_id_to_children.each { |image_id, child_ids|
