42546678822371c7e41bc43f4afea1b34c6969c1
vagrant <me@ktamura.com>
2014-06-08 18:24:21 +0000

    mongodb read script

--- scripts/kiyoto/analysis.sh
+++ scripts/kiyoto/analysis.sh
@@ -4,3 +4,7 @@
 # I assume that this is run from the project root
 
 ruby -e 'STDIN.each_line {|x| puts x.split("/").first}' < data/repo_names.txt | sort | uniq -c | wc -l
+
+# MongoDB Scripts
+
+mongo ds061787.mongolab.com:61787/docker-hackathon -u dockerlove -p dockerlove --eval 'db.raw.metadata.group( { key: { _id: 1}, reduce: function(curr, result) { result.total += curr.Ancestry.length }, initial: { total: 0} }).sort(function(x,y) { return x["total"]-y["total"]}).forEach(function(el){print(el._id+" "+el.total)})'
